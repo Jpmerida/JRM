98b211f161cdef28cd4b17c61e2b46aec0b8e4d3
Kirk Jann V. Garcia <janngarcia24@gmail.com>
2019-03-06 18:27:02 +0800

    form

--- JRM/Module1.vb
+++ JRM/Module1.vb
@@ -9,8 +9,9 @@ Module Module1
     Public dr As MySqlDataReader
     Public conn As MySqlConnection = New MySqlConnection
 
-    Public Sub condb()
+    Public Sub conndb()
         conn.ConnectionString = ("Server = localhost; uid = root; pwd = admin; database = jrm")
+        conn.Open()
 
     End Sub
 End Module
