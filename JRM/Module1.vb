f0a5fb275650bcb1934be2111b1e170b70752b89
Kirk Jann V. Garcia <janngarcia24@gmail.com>
2019-03-06 17:18:35 +0800

    Initial commit

---
+++ JRM/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+Module Module1
+    Public connectionString As String
+    Public mysql As String
+    Public connection As MySqlConnection
+    Public cmd As MySqlCommand
+    Public dr As MySqlDataReader
+    Public conn As MySqlConnection = New MySqlConnection
+
+    Public Sub condb()
+        conn.ConnectionString = ("Server = localhost; uid = root; pwd = admin; database = jrm")
+
+    End Sub
+End Module
